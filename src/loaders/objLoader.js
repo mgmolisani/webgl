a25068e833b07ca7eb207b91c76a09f962fe99ce
Mike Molisani <mgmolisani@gmail.com>
2020-05-28 00:20:20 -0400

    WIP

--- src/loaders/objLoader.js
+++ src/loaders/objLoader.js
@@ -1,24 +1,41 @@
-export const objLoader = (obj) => {
-  const geometries = [null];
-  const normals = [null];
-  const textures = [null];
-  let object = [];
+const load = (obj) => {
+    const positions = [null];
+    const normals = [null];
+    const vertices_ = [];
+    const indices_ = [];
 
-  obj.split(`\n`).forEach(line => {
-    const [key, ...data] = line.split(` `);
+    obj.split(`\n`).forEach(line => {
+      const [key, ...data] = line.split(/\s+/).filter(item => item !== ``);
+      if (key === `v`) {
+        positions.push(data);
+      } else if (key === `vn`) {
+        normals.push(data);
+      } else if (key === `f`) {
+        data.forEach(vertex => {
+          const [p, t, n] = vertex.split(`/`);
+          const found = vertices_.findIndex(v =>
+            v[0] === positions[p] && v[1] === normals[n]
+          );
 
-    if (key === `v`) {
-      geometries.push(data);
-    } else if (key === `vt`) {
-      textures.push(data);
-    } else if (key === `vn`) {
-      normals.push(data);
-    } else if (key === `f`) {
-      data.forEach(vertex => {
-        const [g, t, n] = vertex.split(`/`);
-      })
+          if (found === -1) {
+            indices_.push(vertices_.length);
+            vertices_.push([
+              positions[p],
+              normals[n]
+            ]);
+          } else {
+            indices_.push(found);
+          }
+        });
+      }
+    });
+
+    return {
+      vertices: vertices_.flat(2),
+      indices: indices_,
     }
-  })
+}
 
-  return object;
+export const ObjLoader = {
+  load,
 }
--- src/renderer.js
+++ src/renderer.js
@@ -1,16 +1,71 @@
 import {Mat4} from './math/Mat4';
+import {ObjLoader} from './loaders/objLoader';
+import {object} from './objects/cube';
 
 const createProgram = (gl) => {
   const vertexShaderSource = `
       #version 100
       
       attribute vec3 a_position;
+      attribute vec3 a_normal;
       uniform mat4 u_model;
       uniform mat4 u_view;
       uniform mat4 u_ortho;
+      varying vec3 v_normal;
+      varying vec3 v_world_position;
+      
+      mat4 transpose(mat4 m) {
+        return mat4(m[0][0], m[1][0], m[2][0], m[3][0],
+                    m[0][1], m[1][1], m[2][1], m[3][1],
+                    m[0][2], m[1][2], m[2][2], m[3][2],
+                    m[0][3], m[1][3], m[2][3], m[3][3]);
+      }
+      
+      mat4 inverse(mat4 m) {
+        float
+            a00 = m[0][0], a01 = m[0][1], a02 = m[0][2], a03 = m[0][3],
+            a10 = m[1][0], a11 = m[1][1], a12 = m[1][2], a13 = m[1][3],
+            a20 = m[2][0], a21 = m[2][1], a22 = m[2][2], a23 = m[2][3],
+            a30 = m[3][0], a31 = m[3][1], a32 = m[3][2], a33 = m[3][3],
+      
+            b00 = a00 * a11 - a01 * a10,
+            b01 = a00 * a12 - a02 * a10,
+            b02 = a00 * a13 - a03 * a10,
+            b03 = a01 * a12 - a02 * a11,
+            b04 = a01 * a13 - a03 * a11,
+            b05 = a02 * a13 - a03 * a12,
+            b06 = a20 * a31 - a21 * a30,
+            b07 = a20 * a32 - a22 * a30,
+            b08 = a20 * a33 - a23 * a30,
+            b09 = a21 * a32 - a22 * a31,
+            b10 = a21 * a33 - a23 * a31,
+            b11 = a22 * a33 - a23 * a32,
+      
+            det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
+      
+        return mat4(
+            a11 * b11 - a12 * b10 + a13 * b09,
+            a02 * b10 - a01 * b11 - a03 * b09,
+            a31 * b05 - a32 * b04 + a33 * b03,
+            a22 * b04 - a21 * b05 - a23 * b03,
+            a12 * b08 - a10 * b11 - a13 * b07,
+            a00 * b11 - a02 * b08 + a03 * b07,
+            a32 * b02 - a30 * b05 - a33 * b01,
+            a20 * b05 - a22 * b02 + a23 * b01,
+            a10 * b10 - a11 * b08 + a13 * b06,
+            a01 * b08 - a00 * b10 - a03 * b06,
+            a30 * b04 - a31 * b02 + a33 * b00,
+            a21 * b02 - a20 * b04 - a23 * b00,
+            a11 * b07 - a10 * b09 - a12 * b06,
+            a00 * b09 - a01 * b07 + a02 * b06,
+            a31 * b01 - a30 * b03 - a32 * b00,
+            a20 * b03 - a21 * b01 + a22 * b00) / det;
+      }
       
       void main() {
-        gl_Position = u_ortho * u_view * u_model * vec4(a_position, 1.0);
+        v_normal = a_normal;
+        v_world_position = vec3(u_model * vec4(a_position, 1.0));
+        gl_Position = u_ortho * u_view * vec4(v_world_position, 1.0);
       }
     `;
 
@@ -27,9 +82,28 @@ const createProgram = (gl) => {
       
       precision mediump float;
       
+      uniform vec3 u_light_position;
+      uniform vec3 u_view_position;
+      //uniform vec3 u_color;
+      varying vec3 v_normal;
+      varying vec3 v_world_position;
+      
       void main() {
-        vec3 color = vec3(gl_FragCoord.z);
-        gl_FragColor = vec4(color, 1.0);
+        vec3 model_color = vec3(1.0, 0.5, 0.25);
+        vec3 light_color = vec3(1.0);
+        vec3 ambient = 0.1 * light_color;
+        vec3 normal = normalize(v_normal);
+        vec3 light_direction = normalize(u_light_position - v_world_position);
+        float diff = max(dot(normal, light_direction), 0.0);
+        vec3 diffuse = diff * light_color;
+        float specular_strength = 0.5;
+        vec3 view_direction = normalize(u_view_position - v_world_position);
+        vec3 reflect_direction = reflect(-light_direction, normal);  
+        float shininess = pow(max(dot(view_direction, reflect_direction), 0.0), 256.0);
+        vec3 specular = specular_strength * shininess * light_color;
+        vec3 light = ambient + diffuse + specular;
+        // gl_FragColor = vec4(light * vec3(1.0, 0.5, 0.25), 1.0);
+        gl_FragColor = vec4(model_color, 1.0);
       }
     `;
 
@@ -87,58 +161,42 @@ export const WebGLRenderer = (canvas) => {
 
   const start = () => {
     resizeObserver.observe(canvas);
-
-    const vertices = new Float32Array([
-      0.0, 0.0, 0.0,
-      0.0, 0.0, 1.0,
-      0.0, 1.0, 0.0,
-      0.0, 1.0, 1.0,
-      1.0, 0.0, 0.0,
-      1.0, 0.0, 1.0,
-      1.0, 1.0, 0.0,
-      1.0, 1.0, 1.0,
-    ]);
-
-    const indices = new Uint16Array([
-      0, 6, 4,
-      0, 2, 6,
-      0, 3, 2,
-      0, 1, 3,
-      2, 7, 6,
-      2, 3, 7,
-      4, 6, 7,
-      4, 7, 5,
-      0, 4, 5,
-      0, 5, 1,
-      1, 5, 7,
-      1, 7, 3
-    ]);
-
+    const cube = ObjLoader.load(object);
+    console.log(cube);
+    const vertices = new Float32Array(cube.vertices);
+    const indices = new Uint16Array(cube.indices);
+    const stride = 6 * 4;
     const program = createProgram(gl);
     gl.useProgram(program);
 
     const positionLocation = gl.getAttribLocation(program, `a_position`);
+    const normalLocation = gl.getAttribLocation(program, `a_normal`);
     gl.enableVertexAttribArray(positionLocation);
+    gl.enableVertexAttribArray(normalLocation);
     const vbo = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
     const ebo = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ebo);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, true, stride, 0);
+    gl.vertexAttribPointer(normalLocation, 3, gl.FLOAT, false, stride, 3 * 4);
     const modelLocation = gl.getUniformLocation(program, `u_model`);
-    const translationMatrix = Mat4.translate(-0.5, -0.5, -0.5);
-    const scaleMatrix = Mat4.scale(0.5, 0.5, 0.5);
-    const rotationMatrix = Mat4.rotate(Math.PI / 4, [0.0, 1.0, 0.0]);
-    const modelMatrix = Mat4.multiply(Mat4.multiply(rotationMatrix, scaleMatrix), translationMatrix);
+    const modelMatrix = Mat4.identity();
     gl.uniformMatrix4fv(modelLocation, false, new Float32Array(modelMatrix));
+    const cameraPosition = [3.0, 3.0, 3.0];
+    const lightPosition = [-3.0, 3.0, 3.0];
     const viewLocation = gl.getUniformLocation(program, `u_view`);
-    const viewMatrix = Mat4.lookAt([0.0, 3.0, 3.0], [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
+    const viewMatrix = Mat4.lookAt(cameraPosition, [0.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
     gl.uniformMatrix4fv(viewLocation, false, new Float32Array(viewMatrix));
     const orthoLocation = gl.getUniformLocation(program, `u_ortho`);
     const aspectRatio = canvas.clientWidth / canvas.clientHeight;
     const ortho = Mat4.ortho(-aspectRatio, aspectRatio, -1.0, 1.0, 0.1, 100);
     gl.uniformMatrix4fv(orthoLocation, false, new Float32Array(ortho));
+    const lightPositionLocation = gl.getUniformLocation(program, `u_light_position`);
+    gl.uniform3fv(lightPositionLocation, new Float32Array(lightPosition));
+    const viewPositionLocation = gl.getUniformLocation(program, `u_view_position`);
+    gl.uniform3fv(viewPositionLocation, new Float32Array(cameraPosition));
     step(performance.now());
   };
 
